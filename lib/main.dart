f738c8bfc37896d07cfd07a2e6cb87769615a00f
osama550 <osamakamel758@gmail.com>
2022-12-05 17:43:50 +0200

    fixed defaultNumber in component

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 import 'package:project/bloc_observer.dart';
 import 'package:project/cubit/app_cubit.dart';
-import 'package:project/modules/Confirm%20Payment/Confirm%20Payment_Screen.dart';
-import 'package:project/modules/add%20new%20recipient/add%20new%20recipient_screen.dart';
-import 'package:project/modules/choose_bill/choose_bill.dart';
-import 'package:project/modules/e_Receipt/e-receipt_screen.dart';
-import 'package:project/modules/home/home_screen.dart';
-import 'package:project/modules/payment/electricity_bill.dart';
-import 'package:project/modules/payment/electricity_screen.dart';
-import 'package:project/modules/transfar_money/transfar_money_screen.dart';
 import 'package:sizer/sizer.dart';
+
+import 'modules/payment/electricity_screen.dart';
+
+
 //
 void main() {
+  disabledCapture();
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp
+  ]);
+
   //  to change status bar and icon color
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-        statusBarColor: Colors.white,
-        statusBarIconBrightness: Brightness.dark
-    ),
-  );
+//SystemChrome.setSystemUIOverlayStyle(
+//     const SystemUiOverlayStyle(
+//         statusBarColor: Colors.white,
+//         statusBarIconBrightness: Brightness.dark
+//     ),
+//   );
+
   runApp(const MyApp());
   Bloc.observer = MyBlocObserver();
+
+}
+disabledCapture(){
+  FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
 }
 
 class MyApp extends StatelessWidget {
@@ -41,7 +49,7 @@ class MyApp extends StatelessWidget {
               scaffoldBackgroundColor: Colors.white,
             ),
             debugShowCheckedModeBanner: false,
-            home:ConfirmPaymentScreen() ,
+            home:ServiseScreen() ,
           );
         },
       ),
